e08169f95ec31182a0039a7e989091096baf16c1
Alina <afyumakaeva@domclick.ru>
2021-11-17 19:36:23 +0300

    init

---
+++ src/main/kotlin/com/example/demo/SecurityConfig.kt
@@ -0,0 +1,18 @@
+package com.example.demo
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    
+    override fun configure(http: HttpSecurity) {
+        with(http) {
+            authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+        }
+    }
+}
